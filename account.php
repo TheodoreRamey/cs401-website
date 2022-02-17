98a8b7ce7c69c9faf9d7fc46a95473fcd48cdd2c
TheodoreRamey <theodoreramey@u.boisestate.edu>
2022-02-16 22:09:08 -0700

    Account page

--- account.php
+++ account.php
@@ -7,6 +7,14 @@
   <div class="background_card">
     <div class="content">
       <h2>Account Info</h2>
+      <div id="user_info">
+        <ul>
+          <li><strong>User Email: </strong>fake.email@somesite.com</li>
+          <li><strong>Name: </strong>John Smith</li>
+          <li><strong>Username: </strong>smithigans372</li>
+          <li><strong>Change Password (TBD)</strong></li>
+        </ul>
+      </div>
     </div>
   </div>
 
--- index.php
+++ index.php
@@ -7,15 +7,17 @@
   <div class="background_card">
     <div class="content">
       <h1>Video Game Sales and Ratings Visualized: The 1980's to The Present Day</h1>
-      <p>
-        Like many people my age, video games are a passion of mine. They have come a long way, from simple games like Asteroids to modern hits like Minecraft or Mario Cart. Now more than ever video games have a huge audience, and are able to appeal to people of many varied interests. Looking back at how games have been rated and how well they sold in different regions can tell us a lot about the growth of the industry and its fans. Using regional sales and ratings data we can visualize the history of video games!
-      </p>
-      <p>
-        Using our infographic tool (NOT YET IMPLEMENTED) you are able to visualize the history of video games. Want to see where shooters have sold best? Or role-playing games? Our data visualization tool can help us see where different types of video games thrive, and which markets may have untapped fans of genres, just waiting for a breakout hit. If you want to know who was the best selling publisher in the 1990's we can show you, and even break it down by region.
-      </p>
-      <p>
-        Using the <a href="https://www.kaggle.com/rush4ratio/video-game-sales-with-ratings">"Video Game Sales with Ratings"</a> dataset from Rush Kirubi, we can provide over 11,000 titles to visualize. There are endless combinations to explore, and you are doubtlessly going to find new video games that strike your interest. Don't you want to know why the Yakuza series sold so well inside Japan, but failed to strike it big in the foreign market until their last few games?
-      </p>
+      <div class="normal_text">
+        <p>
+          Like many people my age, video games are a passion of mine. They have come a long way, from simple games like Asteroids to modern hits like Minecraft or Mario Cart. Now more than ever video games have a huge audience, and are able to appeal to people of many varied interests. Looking back at how games have been rated and how well they sold in different regions can tell us a lot about the growth of the industry and its fans. Using regional sales and ratings data we can visualize the history of video games!
+        </p>
+        <p>
+          Using our infographic tool (NOT YET IMPLEMENTED) you are able to visualize the history of video games. Want to see where shooters have sold best? Or role-playing games? Our data visualization tool can help us see where different types of video games thrive, and which markets may have untapped fans of genres, just waiting for a breakout hit. If you want to know who was the best selling publisher in the 1990's we can show you, and even break it down by region.
+        </p>
+        <p>
+          Using the <a href="https://www.kaggle.com/rush4ratio/video-game-sales-with-ratings">"Video Game Sales with Ratings"</a> dataset from Rush Kirubi, we can provide over 11,000 titles to visualize. There are endless combinations to explore, and you are doubtlessly going to find new video games that strike your interest. Don't you want to know why the Yakuza series sold so well inside Japan, but failed to strike it big in the foreign market until their last few games?
+        </p>
+      </div>
     </div>
   </div>
   

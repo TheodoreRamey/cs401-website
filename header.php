85c955706fc87f49b45c2c1396462c863b61da1d
TheodoreRamey <theodoreramey@u.boisestate.edu>
2022-04-18 18:00:47 -0600

    added https to js path to get js working on heroku

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="style.css">
     <title>Data Visualization</title>
     <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://d3js.org/d3.v7.min.js"></script>
   </head>
   <body>
